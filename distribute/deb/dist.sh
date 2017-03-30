dd8548da8a33fe8263b554595574d958d41e3c27
Juergen Weigert <jw@owncloud.com>
2017-03-30 19:42:32 +0200

    distribute scripts for generating a *.deb package added.

---
+++ distribute/deb/dist.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+# Make a debian/ubuntu distribution
+
+name=$1
+vers=$2
+url=http://github.com/fablabnbg/inkscape-paths2openscad
+requires="openscad, cura | bash"
+
+tmp=../out
+
+[ -d $tmp ] && rm -rf $tmp/*.deb
+mkdir $tmp
+cp description-pak files
+cd files
+fakeroot checkinstall --fstrans --reset-uid --type debian \
+  --install=no -y --pkgname $name --pkgversion $vers --arch all \
+  --pkglicense LGPL --pkggroup other --pakdir ../$tmp --pkgsource $url \
+  --pkgaltsource "http://fablab-nuernberg.de" \
+  --maintainer "'Juergen Weigert (juergen@fabmail.org)'" \
+  --requires "'$requires'" make install \
+  -e PREFIX=/usr || { echo "error"; exit 1; }
+
+test -f ../$tmp/*.deb && dpkg-deb -c ../$tmp/*.deb
+
